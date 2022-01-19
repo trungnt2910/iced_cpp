eaf25c5b843e108195563bb37bbde47d370066fa
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2022-01-19 19:07:28 +0700

    feat: Improved, C++-friendly ByteArrayCodeReader

--- iced_cpp/Intel/ByteArrayCodeReader.h
+++ iced_cpp/Intel/ByteArrayCodeReader.h
@@ -20,6 +20,8 @@
 #include <vector>
 
 #include <string_view>
+#include <ranges>
+#include "HexUtils.h"
 
 // Code generated from Iced. Do not edit.
 // Commit tag: badb6147c0994a4954fa27645aba2b02c2bb9502.
@@ -27,59 +29,100 @@
 // Copyright (C) 2018-present iced project and contributors
 namespace Iced::Intel
 {
+	template <typename R>
+	concept byte_random_access_borrowed_range =
+		std::ranges::borrowed_range<R> &&
+		std::ranges::random_access_range<R> &&
+		std::is_convertible_v<std::ranges::range_value_t<R>, std::uint8_t> &&
+		sizeof(std::ranges::range_value_t<R>) == sizeof(std::uint8_t);
+
+	template <typename R>
+	concept byte_range =
+		std::ranges::range<R> &&
+		std::is_convertible_v<std::ranges::range_value_t<R>, std::uint8_t> &&
+		sizeof(std::ranges::range_value_t<R>) == sizeof(std::uint8_t);
+
 	/// <summary>
 	/// A <see cref="CodeReader"/> that reads data from a byte array
 	/// </summary>
+	template <byte_random_access_borrowed_range R = std::string_view>
 	class ByteArrayCodeReader final : public CodeReader
 	{
 		/* readonly */
 	private:
-		std::vector<std::uint8_t> data;
-		std::int32_t currentPosition = 0;
-		/* readonly */
-		std::int32_t startPosition = 0;
-		/* readonly */
-		std::int32_t endPosition = 0;
+		std::vector<std::ranges::range_value_t<R>> copied_data;
+		R data;
+		std::ranges::borrowed_iterator_t<R> current;
 		/// <summary>
 		/// Current position
 		/// </summary>
 	public:
-		std::int32_t GetPosition() const;
-		void SetPosition(std::int32_t value);
+		constexpr std::int32_t GetPosition() const
+		{
+			return (std::int32_t)(current - std::ranges::begin(data));
+		}
+		constexpr void SetPosition(std::int32_t value)
+		{
+			current = std::ranges::begin(data) + value;
+		}
 		/// <summary>
 		/// Number of bytes that can be read
 		/// </summary>
-		std::int32_t GetCount() const;
+		constexpr std::int32_t GetCount() const
+		{
+			return (std::int32_t)(std::ranges::size(data));
+		}
 		/// <summary>
 		/// Checks if it's possible to read another byte
 		/// </summary>
-		bool GetCanReadByte() const;
+		constexpr bool GetCanReadByte() const
+		{
+			return current != std::ranges::end(data);
+		}
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="range">Byte borrowed range, such as std::string_view</param>
+		constexpr ByteArrayCodeReader(const R& range) :
+			data(range),
+			current(std::ranges::begin(data))
+		{
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="range">Any range that contains bytes</param>
+		template <byte_range R1>
+		constexpr ByteArrayCodeReader(const R1& range) : 
+			copied_data(std::ranges::begin(range), std::ranges::end(range)), 
+			data(copied_data.begin(), copied_data.end()), 
+			current(std::ranges::begin(data)) 
+		{
+		}
+
 		/// <summary>
 		/// Constructor
 		/// </summary>
 		/// <param name="hexData">Hex bytes encoded in a string</param>
-		ByteArrayCodeReader(const std::string& hexData);
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		/// <param name="data">Data</param>
-		ByteArrayCodeReader(const std::vector<std::uint8_t>& data);
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		/// <param name="data">Data</param>
-		/// <param name="index">Start index</param>
-		/// <param name="count">Number of bytes</param>
-		ByteArrayCodeReader(const std::vector<std::uint8_t>& data, std::int32_t index, std::int32_t count);
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		/// <param name="data">Data</param>
-		ByteArrayCodeReader(const std::basic_string_view<std::uint8_t>& data);
+		static constexpr ByteArrayCodeReader DecodeFromString(const std::string& hexData)
+		{
+			return ByteArrayCodeReader(HexUtils::ToByteArray(hexData));
+		}
+
 		/// <summary>
 		/// Reads the next byte or returns less than 0 if there are no more bytes
 		/// </summary>
 		/// <returns></returns>
-		std::int32_t ReadByte() override;
+		constexpr std::int32_t ReadByte() override
+		{
+			if (!GetCanReadByte())
+			{
+				return -1;
+			}
+			std::int32_t result = (std::uint8_t)*current;
+			++current;
+			return result;
+		}
 	};
 }
--- iced_cpp/Intel/HexUtils.h
+++ iced_cpp/Intel/HexUtils.h
@@ -29,9 +29,79 @@ namespace Iced::Intel
 {
 	class HexUtils final
 	{
-	public:
-		static std::vector<std::uint8_t> ToByteArray(const std::string& hexData);
 	private:
-		static std::int32_t TryParseHexChar(char c);
+		static constexpr std::int32_t TryParseHexChar(char c)
+		{
+			if ('0' <= c && c <= '9')
+			{
+				return c - '0';
+			}
+			if ('A' <= c && c <= 'F')
+			{
+				return c - 'A' + 10;
+			}
+			if ('a' <= c && c <= 'f')
+			{
+				return c - 'a' + 10;
+			}
+			return -1;
+		}
+	public:
+		static constexpr std::vector<std::uint8_t> ToByteArray(const std::string& hexData)
+		{
+			if (hexData == "")
+			{
+				throw std::invalid_argument("empty: hexData");
+			}
+			if (hexData.length() == 0)
+			{
+				return std::vector<std::uint8_t>();
+			}
+			std::int32_t len = 0;
+			for (std::int32_t i = 0; i < hexData.length(); i++)
+			{
+				if (hexData[i] != ' ')
+				{
+					len++;
+				}
+			}
+			auto data = std::vector<std::uint8_t>(len / 2);
+			std::int32_t w = 0;
+			for (std::int32_t i = 0; ;)
+			{
+				while (i < hexData.length() && std::isspace(hexData[i]))
+				{
+					i++;
+				}
+				if (i >= hexData.length())
+				{
+					break;
+				}
+				std::int32_t hi = TryParseHexChar(hexData[i++]);
+				if (hi < 0)
+				{
+					throw std::invalid_argument("out of range: hexData");
+				}
+				while (i < hexData.length() && std::isspace(hexData[i]))
+				{
+					i++;
+				}
+				if (i >= hexData.length())
+				{
+					throw std::invalid_argument("out of range: hexData");
+				}
+				std::int32_t lo = TryParseHexChar(hexData[i++]);
+				if (lo < 0)
+				{
+					throw std::invalid_argument("out of range: hexData");
+				}
+				data[w++] = static_cast<std::uint8_t>((hi << 4) | lo);
+			}
+			if (w != data.size())
+			{
+				throw std::runtime_error("invalid operation");
+			}
+			return data;
+		}
 	};
 }
